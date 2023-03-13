12deafed9bc7e86df5587c565108673c93b19c78
Scheunemannn <99615216+Scheunemannn@users.noreply.github.com>
2023-03-13 09:06:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-function clicado() {
-    let emaio = document.getElementById('emaio').value
-    // let senha = document.getElementById('senha').value
-    localStorage.setItem('emaio', emaio)
-    let email = localStorage.getItem('emaio')
-    // console.log(senhaa)
-    console.log(email)
-    
 
 
+let clicado = () =>{
+    let emaio = document.getElementById('emaio').value=''
+    let senha = document.getElementById('senha').value=''
 }
 
-
-
+function submitName(e) {
+    if (e.code === 'Enter') {
+        document.getElementById('entrar').click();
+    }
+}
+ 
